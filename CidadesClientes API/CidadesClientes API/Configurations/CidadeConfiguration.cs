a89acf0050ab86559345b292fb61dffda9ee70c3
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2021-12-31 09:54:15 -0300

    Add configuration

---
+++ CidadesClientes API/CidadesClientes API/Configurations/CidadeConfiguration.cs
@@ -0,0 +1,32 @@
+using CidadesClientes_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CidadesClientes_API.Configurations
+{
+    public class CidadeConfiguration : IEntityTypeConfiguration<Cidade>
+    {
+        public void Configure(EntityTypeBuilder<Cidade> builder)
+        {
+            builder.ToTable("Cidades");
+
+            builder
+                .Property(c => c.Id)
+                .HasColumnName("Id")
+                .HasColumnType("uniqueidentifier")
+                .IsRequired();
+
+            builder
+                .Property(c => c.Nome)
+                .HasColumnName("Nome")
+                .HasColumnType("nvarchar(max)")
+                .IsRequired();
+
+            builder
+                .Property(c => c.Estado)
+                .HasColumnName("Estado")
+                .HasColumnType("nvarchar(max)")
+                .IsRequired();
+        }
+    }
+}
