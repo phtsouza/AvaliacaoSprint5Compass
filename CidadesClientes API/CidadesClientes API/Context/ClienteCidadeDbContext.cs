ac686b61c8955a49db9d08393e80c946a6ff37a8
Pedro Henrique Teixeira de Souza <phtsouza@gmail.com>
2022-01-02 16:45:08 -0300

    Add class configuration

--- CidadesClientes API/CidadesClientes API/Context/ClienteCidadeDbContext.cs
+++ CidadesClientes API/CidadesClientes API/Context/ClienteCidadeDbContext.cs
@@ -1,4 +1,5 @@
 using CidadesClientes_API.Models;
+using CidadesClientes_API.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace CidadesClientes_API.Context
@@ -9,6 +10,12 @@ namespace CidadesClientes_API.Context
 
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Cidade> Cidades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CidadeConfiguration());
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+        }
     }
 }
 
